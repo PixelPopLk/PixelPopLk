8b6cd0926f222745287bd0458b98526a9151bff9
PixelPopLk <pasindumadusanka3339@gmail.com>
2026-07-14 21:40:28 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -184,6 +184,15 @@ function RootComponent() {
       {isNotAdmin && <MonetagPopunder />}
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
+
+      {/* 🐉 Real Live Dragon Animation */}
+      <div className="fixed bottom-0 right-0 z-50 pointer-events-none h-40 w-40 sm:h-52 sm:w-52">
+        <iframe 
+          src="https://lottie.host/embed/84e4fdb0-de8b-4a81-9b16-cd3489be4747/9Dnd3P9fJS.json"
+          className="w-full h-full border-0"
+          title="Real Dragon Animation"
+        />
+      </div>
     </QueryClientProvider>
   );
-      }
+}
