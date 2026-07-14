9c2ecde121d45d5d1bebe5743d70b4859388519b
PixelPopLk <pasindumadusanka3339@gmail.com>
2026-07-14 21:30:30 +0530

    Create Dragon.tsx

---
+++ src/components/Dragon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Dragon() {
+  return (
+    <div className="fixed bottom-0 right-0 z-50 pointer-events-none h-40 w-40 sm:h-52 sm:w-52">
+      {/* Real 3D-style Lottie Dragon Animation */}
+      <iframe 
+        src="https://lottie.host/embed/84e4fdb0-de8b-4a81-9b16-cd3489be4747/9Dnd3P9fJS.json"
+        className="w-full h-full border-0"
+        title="Real Dragon Animation"
+      />
+    </div>
+  );
+}
